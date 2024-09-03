f99596813db91b5ac1fc8636d1120dbedf5abb32
Hector Auvinen <hectorauvinen@gmail.com>
2024-09-03 20:14:24 +0300

    more multi-gpu

--- meta-mctg/dcg_meta.py
+++ meta-mctg/dcg_meta.py
@@ -201,6 +201,7 @@ def prep_data(tokenized_data,args,rank,world_size,pin_memory=False,num_workers=0
 
 def train(args,rank,world_size):
     ############## Added #####################################################
+    print(args)
     args.device = rank
     ############## Added #####################################################
     '''
@@ -919,7 +920,7 @@ def main():
 
     ### Added #################################
     world_size = 2
-    mp.spawn(train, args=(world_size, args), nprocs=world_size)
+    mp.spawn(train, args=(args,world_size), nprocs=world_size)
     ### Added #################################
     #train(args)
 
