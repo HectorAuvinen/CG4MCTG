16a3d3c0acb415b8f0883253f659d0a9ff61f589
Hector Auvinen <hectorauvinen@gmail.com>
2024-09-03 21:35:51 +0300

    try float16

--- meta-mctg/dcg_meta.py
+++ meta-mctg/dcg_meta.py
@@ -211,7 +211,7 @@ def train(args):
     config.dcg_att_num = len(label_keys)
     config.dcg_att_len = args.dcg_att_len
     config.dcg_task_len = args.dcg_task_len
-    model = GPT2LMHeadModel.from_pretrained(args.model_name_or_path, config=config,dtype=torch.float16)
+    model = GPT2LMHeadModel.from_pretrained(args.model_name_or_path, config=config,torch_dtype=torch.float16)
 
     # frozen parameters
     for param in model.named_parameters():
