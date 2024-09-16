9556adaf19de076a610de83b0d070f4690f911ff
Hector Auvinen <hectorauvinen@gmail.com>
2024-09-16 12:18:23 +0300

    Update evaluation script

--- evaluation/eval_compmctg.sh
+++ evaluation/eval_compmctg.sh
@@ -1,6 +1,6 @@
 
-dataset_path=./test_cases/cases_Fyelp.jsonl
-dataset=Fyelp
-device_num=0
+dataset_path=../test_data/dcg-Yelp-Original-0-bs=8-epoch=8_seen.jsonl
+dataset=Yelp
+device_num=1
 
 python ./scripts/eval_compmctg.py --dataset_path $dataset_path --dataset $dataset --device_num $device_num
