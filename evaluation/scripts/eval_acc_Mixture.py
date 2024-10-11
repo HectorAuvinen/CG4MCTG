e1a6194252d15bd492eb1afffdb600deb7626d5b
Hector Auvinen <hectorauvinen@gmail.com>
2024-10-11 16:05:39 +0300

    fix perplexity format

--- evaluation/scripts/eval_acc_Mixture.py
+++ evaluation/scripts/eval_acc_Mixture.py
@@ -230,7 +230,7 @@ def main():
     with open("results.txt", "w") as results_file:
         for key in acc_dic.keys():
             results_file.write(f"{key}: {acc_dic[key]}\n")
-        results_file.write(f"perplexity: {perplexity}\n")
+        results_file.write(f"avg_ppl: {perplexity["avg_ppl"]}\n")
     wandb.save("results.txt")
     
     wandb.finish()
