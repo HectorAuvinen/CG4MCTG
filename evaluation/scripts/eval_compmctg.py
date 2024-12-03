bad21bbb2d57a6b24b7aac0f143b09991beb9990
Hector Auvinen <hectorauvinen@gmail.com>
2024-12-03 15:38:36 +0200

    fix token dist bug

--- evaluation/scripts/eval_compmctg.py
+++ evaluation/scripts/eval_compmctg.py
@@ -16,7 +16,9 @@ def eval():
 
     assert args.dataset is not None
     assert args.device_num is not None
-    args_to_pass = ["--dataset_path", args.dataset_path, "--device_num", args.device_num, "--token_dist", args.token_dist]
+    args_to_pass = ["--dataset_path", args.dataset_path, "--device_num", args.device_num]
+    if args.token_dist:
+        args_to_pass.append("--token_dist")
     if args.dataset in args.eval_Fyelp_path:
         subprocess.run(['python', args.eval_Fyelp_path] + args_to_pass)
         subprocess.run(['python', args.eval_perplexity_path] + args_to_pass)
